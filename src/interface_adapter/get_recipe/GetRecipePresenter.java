f8c2dad7d28518765a7bf64186380a0ecff20a55
zhangru <jinny.zhang@mail.utoronto.ca>
2023-10-30 10:04:45 -0400

    GetRecipeOutputBoundary and GetRecipePresenter done

--- src/interface_adapter/get_recipe/GetRecipePresenter.java
+++ src/interface_adapter/get_recipe/GetRecipePresenter.java
@@ -1,3 +1,30 @@
 package interface_adapter.get_recipe;
-public class GetRecipePresenter {
+import interface_adapter.get_recipe.GetReceipeOutputBoundary;
+import interface_adapter.get_recipe.GetRecipeOutputData;
+import interface_adapter.get_recipe.GetRecipeState;
+import interface_adapter.get_recipe.GetRecipeViewModel;
+import use_case.get_recipe.GetRecipeOutputData;
+
+
+public class GetRecipePresenter implements use_case.get_recipe.GetRecipeOutputBoundary {
+    private final GetRecipeViewModel getRecipeViewModel;
+
+    public GetRecipePresenter(GetRecipeViewModel getRecipeViewModel) {
+        this.getRecipeViewModel = getRecipeViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(GetRecipeOutputData name) {
+        GetRecipeState getRecipeState = getRecipeViewModel.getState();
+        getRecipeState.setRecipe(name.getRecipe());
+        this.getRecipeViewModel.setState(getRecipeState);
+        clearViewModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        GetRecipeState getRecipeState = getRecipeViewModel.getState();
+        getRecipeState.setRecipeError(error);
+        getRecipeViewModel.firePropertyChange();
+    }
 }
