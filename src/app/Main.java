b40b7e347c569b4b90b2c7bd38e1b341af18cb22
AKovalyovv <aleksandr.kovalyovv@gmail.com>
2023-11-13 10:40:01 -0500

    Move ViewManagerModel to the general interface_adapter package

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,6 @@
 package app;
 
-import entities.UserFactory;
-import interface_adapter.get_recipe.ViewManagerModel;
+import interface_adapter.ViewManagerModel;
 import interface_adapter.get_recipe.GetRecipeViewModel;
 import view.GetRecipeView;
 import view.ViewManager;
