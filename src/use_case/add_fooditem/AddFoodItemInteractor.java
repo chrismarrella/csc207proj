70960ae277d46a5465cc70391360081e5ef6f72e
zhangru <jinny.zhang@mail.utoronto.ca>
2023-11-14 23:50:38 -0500

    AddFoodItemInteractor, AddFoodItemOutputBoundary, AddFoodItemOutputData done

---
+++ src/use_case/add_fooditem/AddFoodItemInteractor.java
@@ -0,0 +1,32 @@
+package use_case.add_fooditem;
+
+import entities.FoodItem;
+
+
+public class AddFoodItemInteractor implements AddFoodItemInputBoundary {
+    AddFoodItemOutputBoundary addFoodItemPresenter;
+    AddFoodItemDataAccessInterface addFoodItemDataAccessObject;
+    public AddFoodItemInteractor(AddFoodItemOutputBoundary addFoodItemPresenter,
+                                 AddFoodItemDataAccessInterface addFoodItemDataAccessObject) {
+        this.addFoodItemPresenter = addFoodItemPresenter;
+        this.addFoodItemDataAccessObject = addFoodItemDataAccessObject;
+    }
+    @Override
+    public void execute(AddFoodItemInputData addFoodItemInputData) {
+        String ingredient = addFoodItemInputData.getIngredient();
+        Integer year = addFoodItemInputData.getYear();
+        Integer month = addFoodItemInputData.getMonth();
+        Integer day = addFoodItemInputData.getDay();
+        Float amount = addFoodItemInputData.getAmount();
+        FoodItem foodItem = new FoodItem(ingredient, year, month, day, amount);
+
+        addFoodItemDataAccessObject.addItem(foodItem);
+        if (ingredient.isEmpty()) {
+            addFoodItemPresenter.prepareFailView("Ingredient is empty!");
+        }
+        else {
+            addFoodItemDataAccessObject.addItem(foodItem);
+            addFoodItemPresenter.prepareSuccessView();
+        }
+    }
+}
