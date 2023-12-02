3ac06b4f2381bf151cb7f3f68c02cbc6450e3517
AKovalyovv <aleksandr.kovalyovv@gmail.com>
2023-12-02 16:38:32 -0500

    Basic javadocs for the use case application logic classes

--- src/use_case/get_shopping_list/GetShoppingListDataAccessInterface.java
+++ src/use_case/get_shopping_list/GetShoppingListDataAccessInterface.java
@@ -3,7 +3,18 @@ package use_case.get_shopping_list;
 import entities.FoodItem;
 import java.util.List;
 
+/**
+ * Interface for GetShoppingList's DAO
+ */
 public interface GetShoppingListDataAccessInterface {
+    /**
+     * Gets the inventory of the user
+     * @return List of FoodItems from the user's inventory
+     */
     List<FoodItem> getInventory();
+    /**
+     * Standardizes the names of the given food item names
+     * @return List of standardized food item names
+     */
     List<String> standardizeNames(List<String> names);
 }
--- src/use_case/get_shopping_list/GetShoppingListInputBoundary.java
+++ src/use_case/get_shopping_list/GetShoppingListInputBoundary.java
@@ -1,5 +1,12 @@
 package use_case.get_shopping_list;
 
+/**
+ * Interface for GetShoppingList's InputBoundary
+ */
 public interface GetShoppingListInputBoundary {
+    /**
+     * Executes the use case
+     * @param getShoppingListInputData Input data for the use case (the ingredients required for the recipe)
+     */
     void execute(GetShoppingListInputData getShoppingListInputData);
 }
