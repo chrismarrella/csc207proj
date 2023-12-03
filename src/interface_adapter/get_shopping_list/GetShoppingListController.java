17017af5d6051a7afd29034df875e80e18c93c77
AKovalyovv <aleksandr.kovalyovv@gmail.com>
2023-12-03 13:55:22 -0500

    Basic javadoc docs for GetShoppingList interface adapters

--- src/interface_adapter/get_shopping_list/GetShoppingListController.java
+++ src/interface_adapter/get_shopping_list/GetShoppingListController.java
@@ -5,13 +5,24 @@ import use_case.get_shopping_list.GetShoppingListInputData;
 
 import java.util.List;
 
+/**
+ * Controller for GetShoppingList use case
+ */
 public class GetShoppingListController {
     private GetShoppingListInputBoundary getShoppingListInteractor;
 
+    /**
+     * Constructor for GetShoppingListController
+     * @param getShoppingListInteractor GetShoppingListInteractor to be used
+     */
     public GetShoppingListController(GetShoppingListInputBoundary getShoppingListInteractor) {
         this.getShoppingListInteractor = getShoppingListInteractor;
     }
 
+    /**
+     * Executes the GetShoppingList use case by calling the Interactor
+     * @param recipeIngredients Ingredients required for the recipe
+     */
     public void execute(List<String> recipeIngredients) {
         GetShoppingListInputData getShoppingListInputData = new GetShoppingListInputData(recipeIngredients);
         getShoppingListInteractor.execute(getShoppingListInputData);
--- src/interface_adapter/get_shopping_list/GetShoppingListState.java
+++ src/interface_adapter/get_shopping_list/GetShoppingListState.java
@@ -3,20 +3,40 @@ package interface_adapter.get_shopping_list;
 import java.util.List;
 import entities.FoodItem;
 
+/**
+ * State for GetShoppingList use case
+ */
 public class GetShoppingListState {
-    private List<String> groceryItems = null;
+    private List<String> shoppingList = null;
     private String error = null;
 
+    /**
+     * Constructor for GetShoppingListState
+     * @param groceryItems List of grocery items
+     * @param error Error message to be displayed
+     */
     public void updateState(List<String> groceryItems, String error) {
-        this.groceryItems = groceryItems;
+        this.shoppingList = groceryItems;
         this.error = error;
     }
 
+    /**
+     * Sets the error message
+     * @param error Error message to be displayed
+     */
     public void setShoppingListError(String error) {
         this.error = error;
     }
 
+    /**
+     * Getter for error
+     * @return Error message to be displayed
+     */
     public String getError() { return this.error; }
 
-    public List<String> getShoppingList() { return groceryItems; }
+    /**
+     * Getter for groceryItems
+     * @return List of of items in the shopping list
+     */
+    public List<String> getShoppingList() { return shoppingList; }
 }
